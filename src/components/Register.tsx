106c4a1e4dbce261214c2efb4e8b6c3e1571c30d
theeramat sangsin <theeramat1123.ts@gmail.com>
2019-11-06 17:08:16 +0700

    add Structure code

---
+++ src/components/Register.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+class RegisterScreen extends React.Component {
+    render() {
+        return (
+            <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+                <Text style={{ fontSize: 56 }}>
+                    Register page
+                </Text>
+            </View>
+        );
+    }
+}
+
+
+
+export default RegisterScreen
